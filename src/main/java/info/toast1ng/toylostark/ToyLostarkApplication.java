f830e32fa4a0ad9640e1889e89f627802127c0bc
ToasT1ng <jrainboww9@gmail.com>
2023-09-28 02:44:08 +0900

    초기화

---
+++ src/main/java/info/toast1ng/toylostark/ToyLostarkApplication.java
@@ -0,0 +1,13 @@
+package info.toast1ng.toylostark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyLostarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyLostarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/toast1ng/toylostark/ToyLostarkApplicationTests.java
@@ -0,0 +1,13 @@
+package info.toast1ng.toylostark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyLostarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
